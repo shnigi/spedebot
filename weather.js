e8fd5ed90654bb86ff6eee50db44fb92d21be60c
Niki Ahlskog <niki.ahlskog@gmail.com>
2021-01-20 16:18:15 +0200

    weather emojis

--- weather.js
+++ weather.js
@@ -1,12 +1,19 @@
 const fetch = require("node-fetch");
+const emojis = require('./weatherEmojis');
 
 const weather = async (ctx) => {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/group?id=658225,632453&lang=fi&units=metric&appid=${process.env.WEATHER_TOKEN}`)
     const keli = await response.json();
     const [helsinki, vantaa] = keli.list;
+
+// ctx.replyWithPhoto({ url: `http://openweathermap.org/img/w/${vantaa.weather[0].icon}.png` }, { caption: `
+// Helsingissä tänään: ${helsinki.weather[0].description}. Lämpötila on ${helsinki.main.temp}°C ja tuntuu kuin ${helsinki.main.feels_like}°C
+// Vantaalla tänään: ${vantaa.weather[0].description}. Lämpötila on ${vantaa.main.temp}°C ja tuntuu kuin ${vantaa.main.feels_like}°C
+// `});
+
     ctx.reply(`
-Helsingissä tänään: ${helsinki.weather[0].description}. Lämpötila on ${helsinki.main.temp}°C ja tuntuu kuin ${helsinki.main.feels_like}°C
-Vantaalla tänään: ${vantaa.weather[0].description}. Lämpötila on ${vantaa.main.temp}°C ja tuntuu kuin ${vantaa.main.feels_like}°C
+${emojis[helsinki.weather[0].id].icon} Helsingissä tänään: ${helsinki.weather[0].description}. Lämpötila on ${helsinki.main.temp}°C ja tuntuu kuin ${helsinki.main.feels_like}°C
+${emojis[vantaa.weather[0].id].icon} Vantaalla tänään: ${vantaa.weather[0].description}. Lämpötila on ${vantaa.main.temp}°C ja tuntuu kuin ${vantaa.main.feels_like}°C
 `);
 }
 
