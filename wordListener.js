fa2b424b14e08e1980c8b6805386cd61b031d6a3
Niki Ahlskog <niki.ahlskog@gmail.com>
2021-01-22 15:05:52 +0200

    fix

--- wordListener.js
+++ wordListener.js
@@ -19,6 +19,7 @@ const replyToWord = (ctx, word) => {
 const wordListener = (ctx) => wordsToListen
     .filter(word => ctx.message.text
     .toLowerCase()
+    .split(' ')
     .includes(word)
     && replyToWord(ctx, word));
 
