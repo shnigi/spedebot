2be989ee5db77afce872a8a8077720e30f00eeca
Sunphinx <colioris_effectivity@hotmail.com>
2021-01-22 13:43:18 +0200

    Change order of functions

--- wordListener.js
+++ wordListener.js
@@ -1,26 +1,26 @@
 const wordsToListenFor = ['pystytkö', '/kick tommi', 'pizza'];
 
+const replyToWord = (ctx, word) => {
+    switch(word) {
+      case 'pystytkö':
+          ctx.replyWithAudio({ source: './media/PystynVaanenPistä.mp3' })
+          break;
+      case '/kick tommi':
+          ctx.reply('Tommi lensi ulos mutta pingahti takaisin sisälle.')
+          break;
+      case 'pizza':
+          ctx.reply('Pizzaperjantai?')
+          break;
+      default:
+          break;
+      }
+  }
+
 const wordListener = (ctx) => {
     for (let i in wordsToListenFor) {
         if (ctx.message.text.toLowerCase().includes(wordsToListenFor[i])) {
             replyToWord(ctx, wordsToListenFor[i])
         }
-    }
-}
-
-const replyToWord = (ctx, word) => {
-  switch(word) {
-    case 'pystytkö':
-        ctx.replyWithAudio({ source: './media/PystynVaanenPistä.mp3' })
-        break;
-    case '/kick tommi':
-        ctx.reply('Tommi lensi ulos mutta pingahti takaisin sisälle.')
-        break;
-    case 'pizza':
-        ctx.reply('Pizzaperjantai?')
-        break;
-    default:
-        break;
     }
 }
 
