8398959b61b066e386f1560216053ae94c2dd0b0
Niki Ahlskog <niki.ahlskog@gmail.com>
2021-01-27 17:12:59 +0200

    stock suggestions

--- stocks.js
+++ stocks.js
@@ -1,15 +1,32 @@
 const fetch = require("node-fetch");
 
+const getStockSuggestions = async (ctx, stockName) => {
+    const url = `https://finance.yahoo.com/_finance_doubledown/api/resource/searchassist;searchTerm=${stockName}`;
+    const response = await fetch(url);
+    const symbolSuggests = await response.json();
+    const { items } = symbolSuggests;
+    if ( items && items.length === 0 ) {
+        ctx.reply('En osaa edes ehdottaa mitään. Kokeile urpo uudestaan');
+    } else {
+    ctx.reply(`Osaketta ei löydy, olisiko se joku näistä?
+${items.map(stock => `${stock.symbol} \n`).join('')}
+    `);
+    }
+}
 const getStock = async (ctx, stockName) => {
-    const response = await fetch(`https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com&symbols=${stockName}`)
+    const url = `https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com&symbols=${stockName}`;
+    const response = await fetch(url);
     const stokit = await response.json();
     if (stokit.quoteResponse.result.length === 0) {
-        ctx.reply('Osaketta ei löydy, kokeile urpo uudestaan.');
+        getStockSuggestions(ctx, stockName);
     } else {
-        const [currentValue] = stokit.quoteResponse.result
+        const [currentValue] = stokit.quoteResponse.result;
+        const symbol = currentValue.symbol;
+        const regularMarketPrice = (currentValue && currentValue.regularMarketPrice) || 'Arvo ei tiedossa';
+        const regularMarketChangePercent = (currentValue && currentValue.regularMarketChangePercent && currentValue.regularMarketChangePercent.toFixed(2)) || 'Muutos ei tiedossa';
         ctx.reply(`
-${stockName}: ${currentValue.regularMarketPrice}$
-Muutos: ${currentValue.regularMarketChangePercent.toFixed(2)}%
+${symbol}: ${regularMarketPrice}$
+Muutos: ${regularMarketChangePercent}%
 `);
     }
 };
