efb6f3e5844ab4910b98d2904d96a017c3618b0b
Niki Ahlskog <niki.ahlskog@gmail.com>
2021-02-10 14:04:37 +0200

    use username instead of real name

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ bot.command('osake', (ctx) => {
 
 bot.command('stocks', (ctx) => {
   const [notUsed, command, stockname] = ctx.message.text.split(' ');
-  const userName = `${ctx.update.message.from.first_name} ${ctx.update.message.from.last_name}`;
-  console.log('userName', userName);
+  const userName = ctx.update.message.from.username;
+  console.log('ctx.update.message.from', ctx.update.message.from.username);
   if (!command) getUserStocks(ctx, userName);
   else if (command === 'add' && stockname !== '') {
     addStockToUser(ctx, userName, stockname);
