5837c9bbc53c773a02dc400d98c5af8c5bb4097f
Niki Ahlskog <niki.ahlskog@gmail.com>
2021-03-09 11:01:37 +0200

    add duckduck search

--- search.js
+++ search.js
@@ -1,12 +1,27 @@
 const fetch = require('node-fetch');
 
+const searchDuckDuck = async (ctx, search) => {
+    const url = `https://api.duckduckgo.com/?q=${search}&format=json`;
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data && data.RelatedTopics.length > 0) {
+        ctx.reply(`${data.RelatedTopics[0].Text} - ${data.RelatedTopics[0].FirstURL}`);
+    } else {
+        ctx.reply('Ei kyllä löytynyt niin yhtään mitään!');
+    }
+};
+
 const searchWolfram = async (ctx, searchWord) => {
     const search = encodeURI(searchWord.trim());
     const url = `http://api.wolframalpha.com/v1/result?appid=${process.env.WOLFRAM_ALPHA_TOKEN}&i=${search}&units=metric`;
     const response = await fetch(url);
     const data = await response.text();
     if (data) {
-        ctx.reply(data);
+        if (data === 'No short answer available' || data === 'Wolfram|Alpha did not understand your input') {
+            searchDuckDuck(ctx, search);
+        } else {
+            ctx.reply(data);
+        }
     }
 };
 
