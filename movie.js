da5b3d2169837fb5fad8b46a9fce94d79304fbae
Niki Ahlskog <niki.ahlskog@gmail.com>
2021-03-15 15:05:07 +0200

    fix movie

--- movie.js
+++ movie.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 const searchMovie = async (ctx, movieName) => {
     const search = encodeURI(movieName.trim());
-    const url = `http://www.omdbapi.com/?t=${movieName}&apikey=${process.env.OMDB_KEY}`;
+    const url = `http://www.omdbapi.com/?t=${search}&apikey=${process.env.OMDB_KEY}`;
     const response = await fetch(url);
     const data = await response.json();
     if (data && data.Response === 'True') {
