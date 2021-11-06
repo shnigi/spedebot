ea294eb08a574b9631693d647427881f7e7b8aeb
Niki Ahlskog <niki.ahlskog@gmail.com>
2021-11-06 23:31:30 +0200

    weather update

--- weather.js
+++ weather.js
@@ -2,18 +2,26 @@ const fetch = require('node-fetch');
 const emojis = require('./weatherEmojis');
 
 const weather = async (ctx) => {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/group?id=658225,632453&lang=fi&units=metric&appid=${process.env.WEATHER_TOKEN}`);
-  const keli = await response.json();
-  const [helsinki, vantaa] = keli.list;
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/group?id=658225,632453&lang=fi&units=metric&appid=${process.env.WEATHER_TOKEN}`);
+    const keli = await response.json();
+    const [helsinki, vantaa] = keli.list;
 
-  // ctx.replyWithPhoto({ url: `http://openweathermap.org/img/w/${vantaa.weather[0].icon}.png` }, { caption: `
-  // Helsingissä tänään: ${helsinki.weather[0].description}. Lämpötila on ${helsinki.main.temp}°C ja tuntuu kuin ${helsinki.main.feels_like}°C
-  // Vantaalla tänään: ${vantaa.weather[0].description}. Lämpötila on ${vantaa.main.temp}°C ja tuntuu kuin ${vantaa.main.feels_like}°C
-  // `});
+    const sunrise = Intl.DateTimeFormat('fi-FI', { timeStyle: 'short' }).format(new Date(helsinki.sys.sunrise * 1000));
+    const sunset = Intl.DateTimeFormat('fi-FI', { timeStyle: 'short' }).format(new Date(helsinki.sys.sunset * 1000));
+    const { humidity } = helsinki.main;
 
-  ctx.reply(`
-${emojis[helsinki.weather[0].id].icon} Helsingissä tänään: ${helsinki.weather[0].description}. Lämpötila on ${helsinki.main.temp}°C ja tuntuu kuin ${helsinki.main.feels_like}°C
-${emojis[vantaa.weather[0].id].icon} Vantaalla tänään: ${vantaa.weather[0].description}. Lämpötila on ${vantaa.main.temp}°C ja tuntuu kuin ${vantaa.main.feels_like}°C
+    ctx.reply(`
+${emojis[helsinki.weather[0].id].icon} Helsingissä: ${helsinki.weather[0].description}.
+Lämpötila ${helsinki.main.temp}°C
+tuntuu kuin ${helsinki.main.feels_like}°C
+
+${emojis[vantaa.weather[0].id].icon} Vantaalla: ${vantaa.weather[0].description}.
+Lämpötila ${vantaa.main.temp}°C
+tuntuu kuin ${vantaa.main.feels_like}°C
+
+ilmankosteus: ${humidity}%
+Aurinko nousee: ${sunrise}
+Aurinko laskee: ${sunset}
 `);
 };
 
