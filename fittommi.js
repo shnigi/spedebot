ab8406e685362ec2785cf405d45e31229c01e1ec
Niki Ahlskog <niki.ahlskog@gmail.com>
2022-01-30 18:33:23 +0200

    fittommi

--- fittommi.js
+++ fittommi.js
@@ -5,25 +5,30 @@ const getSleepTime = (n) => `${n / 60 ^ 0}:` + n % 60;
 const fitBitTommi = async (ctx) => {
     const date = new Date();
     const formattedDate = date.toISOString().split('T')[0];
-    const url = `https://api.fitbit.com/1.2/user/9ST245/sleep/date/${formattedDate}.json`;
+    const urls = [
+        `https://api.fitbit.com/1.2/user/9ST245/sleep/date/${formattedDate}.json`,
+        `https://api.fitbit.com/1.2/user/9ST245/activities/date/${formattedDate}.json`,
+    ];
+    const data = await Promise.all(urls.map(async (url) => {
+        const resp = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${process.env.FITBITTOMMI}`,
+            },
+        });
+        return resp.json();
+    }));
 
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-type': 'application/json',
-            Authorization: `Bearer ${process.env.FITBITTOMMI}`,
-        },
-    });
-    const data = await response.json();
     if (data) {
-        console.log('data', data);
-        const { efficiency } = data.sleep[0];
-        const sleep = getSleepTime(data.summary.totalMinutesAsleep);
-        const [year, month, day] = data.sleep[0].dateOfSleep.split('-');
+        const { efficiency } = data[0].sleep[0];
+        const sleep = getSleepTime(data[0].summary.totalMinutesAsleep);
+        const [year, month, day] = data[0].sleep[0].dateOfSleep.split('-');
 ctx.reply(`
 ${day}.${month}.${year}
 Nukuttu: ${sleep}
-Tehokkuus: ${efficiency}
+Unen tehokkuus: ${efficiency}
+Askeleet: ${data[1].summary.steps}
 `);
     }
 };
