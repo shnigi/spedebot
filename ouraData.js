bdddf59bd143f986c3ecf86fd8cf6d58a7756aa6
Niki Ahlskog <niki.ahlskog@gmail.com>
2022-02-21 11:53:53 +0200

    update oura

--- ouraData.js
+++ ouraData.js
@@ -19,11 +19,11 @@ const ouraData = async (ctx, token) => {
     if (req[0].readiness.length) {
         const [{ readiness: [readiness] }, { sleep: [sleep] }, { activity: [activity] }] = req;
         const datehack = moment(readiness.summary_date, 'YYYY-MM-DD').add(1, 'days').format('DD.MM.YYYY');
-        const steps = activity?.steps || 'Ei tietoa';
+        const steps = activity.steps || 'Ei tietoa';
 ctx.reply(`${datehack}
-Valmiustaso: ${readiness?.score}
-Unipisteet: ${sleep?.score}
-Nukuttu: ${(sleep?.total / 60 / 60).toFixed(2)} tuntia
+Valmiustaso: ${readiness.score}
+Unipisteet: ${sleep.score}
+Nukuttu: ${(sleep.total / 60 / 60).toFixed(2)} tuntia
 Askeleet: ${steps}
 `);
     } else {
