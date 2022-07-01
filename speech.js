8e00f0d3619de5710f194dc688d4a0571ca6e194
Niki Ahlskog <niki.ahlskog@gmail.com>
2022-07-01 13:00:49 +0300

    update

--- speech.js
+++ speech.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const speech = (ctx, text) => {
     const textToQuery = encodeURI(text);
-    const filePath = path.join(__dirname, './downloads/speech.mp3');
+    const filePath = path.join(__dirname, 'speech.mp3');
     const file = fs.createWriteStream(filePath);
     const baseUrl = 'http://translate.google.com.vn/translate_tts?ie=UTF-8&q=';
     http.get(`${baseUrl}${textToQuery}&tl=fi&client=tw-ob`, (response) => {
