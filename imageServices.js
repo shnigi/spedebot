56b4b4509b45bd2b5d0c1c70309d189940896073
Niki Ahlskog <niki.ahlskog@gmail.com>
2022-10-16 00:47:47 +0300

    image analysis usage

--- imageServices.js
+++ imageServices.js
@@ -1,5 +1,17 @@
 const fetch = require('node-fetch');
 
+const apiKey = process.env.IMAGGA_API;
+const apiSecret = process.env.IMAGGA_SECRET;
+const Authorization = process.env.IMAGGA_AUTH;
+
+const headers = {
+    apiKey,
+    apiSecret,
+    Authorization,
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+};
+
 // const downloadFile = (async (url) => {
 //     const res = await fetch(url);
 //     const fileStream = fs.createWriteStream('./filename.jpg');
@@ -10,31 +22,40 @@ const fetch = require('node-fetch');
 //     });
 // });
 
-const imageRecognitionResults = (ctx, { result }) => {
-    const firstFive = result.tags.filter((_, index) => index < 5);
-    ctx.reply(`Arvelen että kuvassa olisi seuraavaa: ${firstFive.map(item => `
+const getImaggaUsage = async () => {
+    const url = 'https://api.imagga.com/v2/usage';
+
+    try {
+        const response = await fetch(url, {  
+            method: 'GET',
+            headers
+        });
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.log(error.response);
+    }
+}
+
+const imageRecognitionResults = (ctx, { result }, usage) => {
+    const firstFive = result.tags.filter((_, index) => index < 8);
+    ctx.reply(`Kuva-analyysejä käytetty tässä kuussa: ${usage.result.monthly_processed} / ${usage.result.monthly_limit}
+Arvelen että kuvassa olisi seuraavaa: ${firstFive.map(item => `
 ${item.confidence.toFixed(1)}% ${item.tag.en}`).join('')}
 `);
 };
 
 const imageRecognition = async (ctx, imageUrl) => {
-    const apiKey = process.env.IMAGGA_API;
-    const apiSecret = process.env.IMAGGA_SECRET;
-    
     const url = `https://api.imagga.com/v2/tags?image_url=${imageUrl}`;
     
     try {
         const response = await fetch(url, {  
             method: 'GET',
-            headers: {
-                apiKey,
-                apiSecret,
-                Authorization: process.env.IMAGGA_AUTH,
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            }});
+            headers
+        });
         const json = await response.json();
-        imageRecognitionResults(ctx, json);
+        const usage = await getImaggaUsage();
+        imageRecognitionResults(ctx, json, usage);
     } catch (error) {
         console.log(error.response);
     }
