51e42594db6cb9cb28141197b6ee805838526ea9
Niki Ahlskog <niki.ahlskog@gmail.com>
2022-11-02 16:53:53 +0200

    update anime api

--- anime.js
+++ anime.js
@@ -2,13 +2,14 @@ const fetch = require('node-fetch');
 
 const searchAnime = async (ctx, animeName) => {
     const search = encodeURI(animeName.trim());
-    const url = `https://api.jikan.moe/v3/search/anime?q=${search}&limit=3`;
+    const url = `https://api.jikan.moe/v4/anime?limit=3&q=${search}`;
     const response = await fetch(url);
-    const data = await response.json();
+    const { data } = await response.json();
     if (data) {
-        ctx.reply(`${data.results.map((anime) => `${anime.title}
-Arvio: ${anime.score}
-${anime.url}\n`).join('')}`);
+        ctx.reply(`${data.map((anime) => `${anime.title}
+Arvio: ${anime.score ?? '-'}
+${anime.url}
+\n`).join('')}`);
     } else {
         ctx.reply('Animea ei löytynyt.');
     }
