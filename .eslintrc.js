c80fab406f647c4a7030023327c8e553aeb93f16
Niki Ahlskog <niki.ahlskog@gmail.com>
2022-12-20 09:46:45 +0200

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,14 @@
 module.exports = {
-    env: {
-        browser: false,
-        commonjs: true,
-        es2021: true,
-    },
-    extends: [
-        'airbnb-base',
-    ],
-    parserOptions: {
-        ecmaVersion: 12,
-    },
-    rules: {
-        'linebreak-style': 'off',
-        indent: ['error', 4],
-        'max-len': ['warn', 150],
-        camelcase: 'off',
-        'no-unused-vars': 'off',
-    },
+  env: {
+    browser: false,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'max-len': ['warn', 100],
+  },
 };
--- anime.js
+++ anime.js
@@ -1,18 +1,24 @@
 const fetch = require('node-fetch');
 
 const searchAnime = async (ctx, animeName) => {
-    const search = encodeURI(animeName.trim());
-    const url = `https://api.jikan.moe/v4/anime?limit=3&q=${search}`;
-    const response = await fetch(url);
-    const { data } = await response.json();
-    if (data) {
-        ctx.reply(`${data.map((anime) => `${anime.title}
+  const search = encodeURI(animeName.trim());
+  const url = `https://api.jikan.moe/v4/anime?limit=3&q=${search}`;
+  const response = await fetch(url);
+  const { data } = await response.json();
+  if (data) {
+    ctx.reply(
+      `${data
+        .map(
+          (anime) => `${anime.title}
 Arvio: ${anime.score ?? '-'}
 ${anime.url}
-\n`).join('')}`);
-    } else {
-        ctx.reply('Animea ei löytynyt.');
-    }
+\n`,
+        )
+        .join('')}`,
+    );
+  } else {
+    ctx.reply('Animea ei löytynyt.');
+  }
 };
 
 module.exports = searchAnime;
