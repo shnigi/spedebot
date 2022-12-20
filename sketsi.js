85284191e408556f6b192483d838efe053fbfa8c
Niki Ahlskog <niki.ahlskog@gmail.com>
2022-12-20 09:34:25 +0200

    update sketsi

--- sketsi.js
+++ sketsi.js
@@ -1,10 +1,10 @@
 const fetch = require('node-fetch');
 
 const sketsi = async (ctx) => {
-  const response = await fetch('https://meme-api.herokuapp.com/gimme');
-  const json = await response.json();
+    const response = await fetch('https://meme-api.com/gimme');
+    const json = await response.json();
 
-  ctx.replyWithPhoto({ url: json.url });
+    ctx.replyWithPhoto({ url: json.url });
 };
 
 module.exports = sketsi;
