76f87a8490e795c254fc42fd06e423ca22bc5fc5
Niki Ahlskog <niki.ahlskog@gmail.com>
2023-01-05 13:46:46 +0200

    update AI

--- openai.js
+++ openai.js
@@ -1,4 +1,4 @@
-const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -6,54 +6,52 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (ctx, query) => {
-    console.log('starting openai image generation');
-    const imagineText = encodeURI(query.trim());
-    try {
-        const response = await openai.createImage({
-            prompt: imagineText,
-            n: 1,
-            size: "1024x1024",
-          });
-          image_url = response.data.data[0].url;
-          console.log('image_url', image_url);
-          ctx.replyWithPhoto({ url: image_url });
-      } catch (error) {
-        if (error.response) {
-          ctx.reply('Quota täynnä tai jotain meni pieleen')
-          console.log(error.response.status);
-          console.log(error.response.data);
-        } else {
-          ctx.reply('Quota täynnä tai jotain meni pieleen')
-          console.log(error.message);
-        }
-      }
+  console.log('starting openai image generation');
+  const imagineText = encodeURI(query.trim());
+  try {
+    const response = await openai.createImage({
+      prompt: imagineText,
+      n: 1,
+      size: '1024x1024',
+    });
+    const imageUrl = response.data.data[0].url;
+    console.log('imageUrl', imageUrl);
+    ctx.replyWithPhoto({ url: imageUrl });
+  } catch (error) {
+    if (error.response) {
+      ctx.reply('Quota täynnä tai jotain meni pieleen');
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      ctx.reply('Quota täynnä tai jotain meni pieleen');
+      console.log(error.message);
+    }
+  }
 };
 
 const shortChat = async (ctx, query) => {
-    const text = encodeURI(query.trim());
-    try {
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `Human: ${text}`,
-            temperature: 0.9,
-            max_tokens: 150,
-            top_p: 1,
-            frequency_penalty: 0.0,
-            presence_penalty: 0.6,
-            stop: [" Human:", " AI:"],
-          });
-          console.log('chat', response);
-          ctx.reply(response.data.choices[0].text);
-      } catch (error) {
-        if (error.response) {
-          ctx.reply('Quota täynnä tai jotain meni pieleen')
-          console.log(error.response.status);
-          console.log(error.response.data);
-        } else {
-          ctx.reply('Quota täynnä tai jotain meni pieleen')
-          console.log(error.message);
-        }
-      }
+  try {
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `Human: ${query}`,
+      temperature: 0.9,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.6,
+      stop: [' Human:', ' AI:'],
+    });
+    ctx.reply(response.data.choices[0].text);
+  } catch (error) {
+    if (error.response) {
+      ctx.reply('Quota täynnä tai jotain meni pieleen');
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      ctx.reply('Quota täynnä tai jotain meni pieleen');
+      console.log(error.message);
+    }
+  }
 };
 
 module.exports = { generateImage, shortChat };
