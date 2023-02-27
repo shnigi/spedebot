050a1de285d6c2b30e31f26c63a84b3e03119763
Niki Ahlskog <niki.ahlskog@gmail.com>
2023-02-27 11:21:01 +0200

    calculate day length

--- weather.js
+++ weather.js
@@ -1,16 +1,28 @@
 const fetch = require('node-fetch');
+const { intervalToDuration } = require('date-fns');
 const emojis = require('./weatherEmojis');
 
 const weather = async (ctx) => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/group?id=658225,632453&lang=fi&units=metric&appid=${process.env.WEATHER_TOKEN}`);
-    const keli = await response.json();
-    const [helsinki, vantaa] = keli.list;
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/group?id=658225,632453&lang=fi&units=metric&appid=${process.env.WEATHER_TOKEN}`,
+  );
+  const keli = await response.json();
+  const [helsinki, vantaa] = keli.list;
 
-    const sunrise = Intl.DateTimeFormat('fi-FI', { timeStyle: 'short' }).format(new Date(helsinki.sys.sunrise * 1000));
-    const sunset = Intl.DateTimeFormat('fi-FI', { timeStyle: 'short' }).format(new Date(helsinki.sys.sunset * 1000));
-    const { humidity } = helsinki.main;
+  const sunriseDate = new Date(helsinki.sys.sunrise * 1000);
+  const sunsetDate = new Date(helsinki.sys.sunset * 1000);
 
-    ctx.reply(`
+  const sunrise = Intl.DateTimeFormat('fi-FI', { timeStyle: 'short' }).format(sunriseDate);
+  const sunset = Intl.DateTimeFormat('fi-FI', { timeStyle: 'short' }).format(sunsetDate);
+
+  const { hours, minutes } = intervalToDuration({
+    start: sunsetDate,
+    end: sunriseDate,
+  });
+
+  const { humidity } = helsinki.main;
+
+  ctx.reply(`
 ${emojis[helsinki.weather[0].id].icon} Helsingissä: ${helsinki.weather[0].description}.
 Lämpötila ${helsinki.main.temp}°C
 tuntuu kuin ${helsinki.main.feels_like}°C
@@ -22,6 +34,7 @@ tuntuu kuin ${vantaa.main.feels_like}°C
 ilmankosteus: ${humidity}%
 Aurinko nousee: ${sunrise}
 Aurinko laskee: ${sunset}
+Päivän pituus: ${hours}h ${minutes}min
 `);
 };
 
