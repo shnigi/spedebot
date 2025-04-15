b495b40968ff6c862e13b82b1b1d12500439de18
Niki Ahlskog <niki.ahlskog@gmail.com>
2025-04-15 13:43:26 +0300

    eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,27 @@
 module.exports = {
   env: {
-    browser: false,
+    node: true,
     commonjs: true,
     es2021: true,
   },
-  extends: ['airbnb-base'],
+  extends: [
+    'airbnb-base',
+    'prettier', // Disables ESLint rules that conflict with Prettier
+  ],
+  plugins: [
+    'prettier', // Enables Prettier as an ESLint rule
+  ],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
-    'max-len': ['warn', 100],
+    'prettier/prettier': 'error', // Show Prettier errors as ESLint errors
+    'no-console': 'off', // Allow console.log since this is a Node.js application
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Ignore variables starting with underscore
+    camelcase: 'off', // Allow non-camelcase for variables
+    'max-len': ['error', { code: 100, ignoreUrls: true }], // Set maximum line length
+    'no-param-reassign': 'off', // Allow parameter reassignment
+    'prefer-destructuring': 'warn', // Prefer destructuring but don't enforce
+    'global-require': 'off', // Allow require statements anywhere
   },
 };
